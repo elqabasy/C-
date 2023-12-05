fbb8f0cafa278c5b2eb9565e80dc55431804ba71
elqabasy <mahros.elqabasy@hotmail.com>
2023-12-05 15:41:07 +0200

    Package - Commify

---
+++ package/main.cpp
@@ -0,0 +1,25 @@
+#include "sources/package.cpp"
+
+
+int main(void){
+	// commify test cases
+	cout << commify(9, ' ') << endl;
+	cout << commify(99, ' ') << endl;
+	cout << commify(999, ' ') << endl;
+	cout << commify(9999, ' ') << endl;
+	cout << commify(99999, ' ') << endl;
+	cout << commify(999999, ' ') << endl;
+	cout << commify(9999999, ' ') << endl;
+	cout << commify(99999999, ' ') << endl;
+	cout << commify(999999999, ' ') << endl;
+	cout << commify(9999999999, ' ') << endl;
+	cout << commify(99999999999, ' ') << endl;
+	cout << commify(999999999999, ' ') << endl;
+	cout << commify(9999999999999, ' ') << endl;
+	cout << commify(99999999999999, ' ') << endl;
+	cout << commify(999999999999999, ' ') << endl;
+	cout << commify(9999999999999999, ' ') << endl;
+	cout << commify(99999999999999999, ' ') << endl;
+
+	return 0;
+}
---
+++ package/sources/package.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+
+/* Commify
+  - Add commas between big numbers
+*/
+
+string commify(unsigned long long int number, char sep=','){
+	string text;
+	int cnt = 0;
+	do{
+		text.insert(0, 1, char('0' + number % 10));
+		number /= 10;
+		if (++cnt == 3 && number){
+			text.insert(0, 1, sep);
+			cnt = 0;
+		}
+	} while (number);
+	return text;
+}
